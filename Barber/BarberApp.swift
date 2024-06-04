43db202898e24b6328504fe303ff77c270e34847
Egor <mironove55@icloud.com>
2024-06-04 17:43:59 +0300

    Initial Commit

---
+++ Barber/BarberApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarberApp.swift
+//  Barber
+//
+//  Created by Gleb  on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
